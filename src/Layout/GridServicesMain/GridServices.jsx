656e98a94b94dd55bd0e534d761381a2b52f98d4
CarlosMadrid10 <carloseduardomadrid.2@gmail.com>
2023-05-22 12:48:44 -0300

    Atualização de Grid

---
+++ src/Layout/GridServicesMain/GridServices.jsx
@@ -0,0 +1,39 @@
+import {Container} from "react-bootstrap";
+import "../../Styles/GridService.css"
+const GridServices = () => {
+    return (
+        <div className="background_grid_service">
+            <Container>
+                <h1 className="title_grid_service">
+                    O que eu Preciso?
+                </h1>
+                <div className="main_service_grid">
+                    <a href="#" className="item_service-grid">1</a>
+                    <a href="#" className="item_service-grid">2</a>
+                    <a href="#" className="item_service-grid">3</a>
+                    <a href="#" className="item_service-grid">4</a>
+                    <a href="#" className="item_service-grid">5</a>
+                    <a href="#" className="item_service-grid">6</a>
+                    <a href="#" className="item_service-grid">7</a>
+
+                    <a href="#" className="item_service-grid">8</a>
+                    <a href="#" className="item_service-grid">9</a>
+                    <a href="#" className="item_service-grid">10</a>
+                    <a href="#" className="item_service-grid">11</a>
+                    <a href="#" className="item_service-grid">12</a>
+                    <a href="#" className="item_service-grid">13</a>
+                    <a href="#" className="item_service-grid">14</a>
+
+                    <a href="#" className="item_service-grid">15</a>
+                    <a href="#" className="item_service-grid">16</a>
+                    <a href="#" className="item_service-grid">17</a>
+                    <a href="#" className="item_service-grid">18</a>
+                </div>
+
+            </Container>            
+        </div>
+
+    );
+}
+ 
+export default GridServices;
