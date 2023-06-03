166c14fc1a98beb55b4f1741cc30c29ee7ea6dca
Lucas <lucas14guesser@gmail.com>
2023-06-03 00:21:26 -0300

    updt style docs

--- src/Pages/DocsProcessos.jsx
+++ src/Pages/DocsProcessos.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+
 import { TreeView, TreeItem } from '@mui/lab';
 import {  FolderOpen, InsertDriveFile, ExpandMore, ChevronRight, UploadFile } from '@mui/icons-material';
-import ImageIcon from '@mui/icons-material/Image';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
+
+import '../Styles/DocsProcessos.css'
 
 const DocsProcessos = () => {
   return (
-    <div>
+    <div className='docs_proccess_container'>
+      <div className="main_docs_proccess">      
       <TreeView
         aria-label="file system navigator"
         defaultCollapseIcon={<ExpandMore />}
@@ -17,20 +21,20 @@ const DocsProcessos = () => {
                 <TreeItem nodeId='3' label="Logos" >
                     <TreeItem nodeId='4' label="Marca de São José">
                         <TreeItem nodeId='5' label="ai">
-                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/ai/logo-sao_jose.ai"><TreeItem nodeId='100' label="logo-sao_jose.ai" icon={<ImageIcon />} /><TreeItem nodeId='101'></TreeItem></a>
+                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/ai/logo-sao_jose.ai"><TreeItem nodeId='100' label="logo-sao-jose.ai" icon={<ImageOutlinedIcon />} /><TreeItem nodeId='101'></TreeItem></a>
                         </TreeItem>
                         <TreeItem nodeId='6' label="eps">
-                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-02.eps"><TreeItem nodeId='102' label="logo-sao_jose-02.eps" icon={<ImageIcon />} /><TreeItem nodeId='103'></TreeItem></a>
-                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-03.eps"><TreeItem nodeId='104' label="logo-sao_jose-03.eps" icon={<ImageIcon />} /><TreeItem nodeId='105'></TreeItem></a>
-                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-04.eps"><TreeItem nodeId='106' label="logo-sao_jose-04.eps" icon={<ImageIcon />} /><TreeItem nodeId='107'></TreeItem></a>
-                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-05.eps"><TreeItem nodeId='108' label="logo-sao_jose-05.eps" icon={<ImageIcon />} /><TreeItem nodeId='109'></TreeItem></a>
-                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-06.eps"><TreeItem nodeId='110' label="logo-sao_jose-06.eps" icon={<ImageIcon />} /><TreeItem nodeId='111'></TreeItem></a>
-                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-07.eps"><TreeItem nodeId='112' label="logo-sao_jose-07.eps" icon={<ImageIcon />} /><TreeItem nodeId='113'></TreeItem></a>
+                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-02.eps"><TreeItem nodeId='102' label="logo-sao-jose-02.eps" icon={<ImageOutlinedIcon />} /><TreeItem nodeId='103'></TreeItem></a>
+                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-03.eps"><TreeItem nodeId='104' label="logo-sao-jose-03.eps" icon={<ImageOutlinedIcon />} /><TreeItem nodeId='105'></TreeItem></a>
+                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-04.eps"><TreeItem nodeId='106' label="logo-sao-jose-04.eps" icon={<ImageOutlinedIcon />} /><TreeItem nodeId='107'></TreeItem></a>
+                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-05.eps"><TreeItem nodeId='108' label="logo-sao-jose-05.eps" icon={<ImageOutlinedIcon />} /><TreeItem nodeId='109'></TreeItem></a>
+                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-06.eps"><TreeItem nodeId='110' label="logo-sao-jose-06.eps" icon={<ImageOutlinedIcon />} /><TreeItem nodeId='111'></TreeItem></a>
+                            <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/eps/logo-sao_jose-07.eps"><TreeItem nodeId='112' label="logo-sao-jose-07.eps" icon={<ImageOutlinedIcon />} /><TreeItem nodeId='113'></TreeItem></a>
                         </TreeItem>
                         <TreeItem nodeId='7' label="jpeg">
 
                         </TreeItem>
-                        <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/logo-sao_jose.rar"><TreeItem nodeId='8' label="logo-sao_jose.rar" icon={<UploadFile />} /><TreeItem nodeId='25'></TreeItem></a>
+                        <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Marca%20de%20S%C3%A3o%20Jos%C3%A9/logo-sao_jose.rar"><TreeItem nodeId='8' label="logo-sao-jose.rar" icon={<UploadFile />} /><TreeItem nodeId='25'></TreeItem></a>
                         <TreeItem nodeId='9' label="pdf">
                         
                         </TreeItem>
@@ -41,7 +45,7 @@ const DocsProcessos = () => {
 
                         </TreeItem>
                     </TreeItem>
-                    <TreeItem nodeId='7' label="Prefeitura de São José">
+                    <TreeItem nodeId='' label="Prefeitura de São José">
                         <TreeItem nodeId='12' label="ai">
 
                         </TreeItem>
@@ -54,7 +58,7 @@ const DocsProcessos = () => {
                         <TreeItem nodeId='15' label="pdf">
 
                         </TreeItem>
-                        <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Prefeitura%20de%20S%C3%A3o%20Jos%C3%A9/logo-prefeitura_sj.rar"><TreeItem nodeId='16' label="logo-prefeitura_sj.rar" icon={<UploadFile />} /><TreeItem nodeId='23'></TreeItem></a>
+                          <a href="https://saojose.sc.gov.br/downloads/Comunica%C3%A7%C3%A3o/Kit%20Imprensa/Logos/Prefeitura%20de%20S%C3%A3o%20Jos%C3%A9/logo-prefeitura_sj.rar"><TreeItem nodeId='16' label="logo-prefeitura_sj.rar" icon={<UploadFile />} /><TreeItem nodeId='23'></TreeItem></a>
                         <TreeItem nodeId='17' label="png">
 
                         </TreeItem>
@@ -73,6 +77,7 @@ const DocsProcessos = () => {
           </TreeItem>
         </TreeItem>
       </TreeView>
+      </div>
     </div>
   );
 };
