873315049086a1cb72b0d2f47a8114ee03a3ab19
Lucas Guesser <lucas14guesser@gmail.com>
2023-06-10 14:29:33 -0300

    updt pag carta servicos

--- src/Layout/NavBar/CartaServicoComponent.jsx
+++ src/Layout/NavBar/CartaServicoComponent.jsx
@@ -1,4 +1,4 @@
-import { NavDropdown, Nav } from 'react-bootstrap';
+import {  Nav } from 'react-bootstrap';
 
 import '../../Styles/NavBar2.css'
 
@@ -6,23 +6,9 @@ const CartaServicoComponent = () => {
 
   return (
     <>
-      <NavDropdown title="Carta Serviços">
-        <Nav.Link href="#">Serviços Oferecidos PMSJ</Nav.Link>
-        <Nav.Link href="#">Serviços Administração</Nav.Link>
-        <Nav.Link href="#">Serviços Assistência Social</Nav.Link>
-        <Nav.Link href="#">Serviços Comunicação</Nav.Link>
-        <Nav.Link href="#">Serviços Cultura</Nav.Link>
-        <Nav.Link href="#">Serviços Educação</Nav.Link>
-        <Nav.Link href="#">Serviços Esportes</Nav.Link>
-        <Nav.Link href="#">Serviços Governo</Nav.Link>
-        <Nav.Link href="#">Serviços Infraestrutura</Nav.Link>
-        <Nav.Link href="#">Serviços Meio Ambiente</Nav.Link>
-        <Nav.Link href="#">Serviços Receita</Nav.Link>
-        <Nav.Link href="#">Serviços Segurança - Transito - Defesa Civil e Procon</Nav.Link>
-        <Nav.Link href="#">Serviços Saúde</Nav.Link>
-        <Nav.Link href="#">Secretaria de Urbanismo e Serviços Públicos</Nav.Link>
-        <Nav.Link href="#">Serviços Transparência</Nav.Link>
-      </NavDropdown>
+      <Nav.Item>
+          <Nav.Link href='/servicos-pmsj' eventKey="link-2">Carta Serviços</Nav.Link>
+      </Nav.Item>
     </>
   )
 }
--- src/Pages/ServicosPMSJ.jsx
+++ src/Pages/ServicosPMSJ.jsx
@@ -11,7 +11,27 @@ const ServicosPMSJ = () => {
             <div className="text_servSj">
                 <Link to="/">Home</Link>
 
-                <h4>Direto no banco</h4>
+                <h4>Carta Serviços</h4>
+
+                <h6>Links:</h6>
+
+                <ul>
+                  <li><a href="https://saojose.sc.gov.br/servicos-oferecidos-pmsj/">Serviços Oferecidos PMSJ</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-de-administracao/servicos-secretaria-de-administracao/">Serviços Administração</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-de-assistencia-social/servicos-assistencia/">Serviços Assistência</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-executiva-de-comunicacao-social/servicos-secretaria-executiva-de-comunicacao-social/">Serviços Comunicação</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-municipal-de-cultura-e-turismo/servicos-fundacao-municipal-de-cultura-e-turismo/">Serviços Cultura</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-de-educacao/servicos-educacao/">Serviços Educação</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/fundacao-municipal-de-esporte-e-lazer/servicos-esportes/">Serviços Esporte</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-da-casa-civil/servicos-casa-civil/">Serviços Governo</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-de-infraestrutura/servicos-infraestrutura/">Serviços Infraestrutura</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/fundacao-municipal-do-meio-ambiente-e-desenvolvimento-sustentavel/servicos-meio-ambiente/">Serviços Meio Ambiente</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-de-receita/servicos-receita/">Serviços Receita</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-de-seguranca-defesa-social-e-transito/servicos-seguranca-transito-defesa-civil-e-procon/">Serviços Segurança - Trânsito - Defesa Civil e Procon</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-de-saude/servicos-saude/">Serviços Saúde</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-de-urbanismo-e-servicos-publicos/">Serviços Urbanismo e Serviços Públicos</a></li>
+                  <li><a href="https://saojose.sc.gov.br/category/secretaria-executiva-de-controle-interno-e-transparencia/servicos-transparencia/">Serviços Transparência</a></li>
+                </ul>
             </div>
         </div>
     </div>
